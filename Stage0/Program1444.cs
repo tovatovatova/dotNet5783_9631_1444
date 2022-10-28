21d972d385783584e2ab8793b1fc0228f4a7d4ae
טובה רוטנשטיין <trottens@g.jct.ac.il>
2022-10-28 14:27:19 +0300

    initial code+welcome function

---
+++ Stage0/Program1444.cs
@@ -0,0 +1,18 @@
+﻿namespace Stage0 {
+    partial class Program
+    {
+        private static void Main(string[] args)
+        {
+            Welcome1444();
+            Console.ReadKey();
+        }
+        static partial void Welcome9631();
+    
+    private static void Welcome1444()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0},welcome to my first console application");
+        }
+    }
+}
