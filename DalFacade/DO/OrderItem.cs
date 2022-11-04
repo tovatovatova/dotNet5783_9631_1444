4f4eec6596ad6104b541b7d44592bab7229742be
טובה רוטנשטיין <trottens@g.jct.ac.il>
2022-11-04 11:29:40 +0200

    comments of variables

--- DalFacade/DO/OrderItem.cs
+++ DalFacade/DO/OrderItem.cs
@@ -7,11 +7,20 @@ namespace DO;
 public struct OrderItem
 {
     /// <summary>
-    /// 
+    /// Product's identification number
     /// </summary>
     public int ProductId { get; set; }
+    /// <summary>
+    /// Order identification number
+    /// </summary>
     public int OrderId { get; set; }
+    /// <summary>
+    ///Price of the Item in Order
+    /// </summary>
     public double price { get; set; }
+    /// <summary>
+    /// The quantity of this item in the order
+    /// </summary>
     public int amount { get; set; }
 
 
--- DalFacade/DO/Product.cs
+++ DalFacade/DO/Product.cs
@@ -1,12 +1,26 @@
 ﻿
 
 namespace DO;
-
+/// <summary>
+/// Structure for detail of product in the store
+/// </summary>
 public struct Product
 {
+    /// <summary>
+    ///Unique identification number for product in store
+    /// </summary>
     public int Id { get; set; }
+    /// <summary>
+    /// Name of product
+    /// </summary>
     public string? Name { get; set; }
+    /// <summary>
+    /// Price of the product
+    /// </summary>
     public double Price { get; set; }
+    /// <summary>
+    /// Boolean variable with value true if the product is avaliable to order and false else
+    /// </summary>
     public bool InStock  { get; set; }
     public override string ToString()=>$@"
 //        
