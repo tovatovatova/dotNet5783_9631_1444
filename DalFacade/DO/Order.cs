c79b2c8cd6216bea822b56ee3fcf0e21266aca06
טובה רוטנשטיין <trottens@g.jct.ac.il>
2022-11-04 11:06:41 +0200

    start comments

--- DalFacade/DO/Order.cs
+++ DalFacade/DO/Order.cs
@@ -3,8 +3,35 @@
 namespace DO;
 
 /// <summary>
-/// 
+/// Structure for products Order details 
 /// </summary>
 public struct Order
 {
+    /// <summary>
+    ///  Unique identification number of each products order
+    /// </summary>
+    public int OrderId { get; set; }
+    /// <summary>
+    /// Name of the order's customer
+    /// </summary>
+    public string? CustomerName { get; set; }
+    /// <summary>
+    /// Email address of the order's customer
+    /// </summary>
+    public string? CustomerEmail { get; set; }
+    /// <summary>
+    /// Order creation Date
+    /// </summary>
+    public DateTime? OrderDate { get; set; }
+    /// <summary>
+    /// Delivery order departure date
+    /// </summary>
+    public DateTime? ShipDate { get; set; }
+    /// <summary>
+    /// Order arrival date
+    /// </summary>
+    public DateTime? DeliveryDate { get; set; }
+
+    
 }
+
