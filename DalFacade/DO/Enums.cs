2edb3b8de1707fc77b7794f0e686c120527da468
טובה רוטנשטיין <trottens@g.jct.ac.il>
2022-11-06 12:52:36 +0200

    readonly random

--- DalFacade/DO/Enums.cs
+++ DalFacade/DO/Enums.cs
@@ -4,5 +4,5 @@ namespace DO;
 
 public struct Enums
 {
-
+    
 }
--- DalList/DataSource.cs
+++ DalList/DataSource.cs
@@ -1,8 +1,12 @@
 ﻿
 
+using System.Globalization;
+
 namespace Dal;
 
 internal static class DataSource
 {
-
+    static readonly Random numbers=new Random();
+    
+    
 }
