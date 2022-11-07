b5a41d3d9bf25c3352c3b71c0515c53a07679b31
טובה רוטנשטיין <trottens@g.jct.ac.il>
2022-11-07 17:01:58 +0200

    a

--- DalList/DataSource.cs
+++ DalList/DataSource.cs
@@ -1,12 +1,20 @@
 ﻿
-
-using System.Globalization;
+using DO;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
 
 namespace Dal;
 
-internal static class DataSource
+internal sealed class DataSource
 {
     static readonly Random numbers=new Random();
- 
-    
+    internal List<Product?> ProductList { get; } = new List<Product?>();
+    internal List<Order?> OrderList { get; } = new List<Order?>();
+    internal List<OrderItem?> OrderItemList { get; } = new List<OrderItem?>();
+    private void addProduct(string? idNum,string? nameProd,double priceProd,bool inStockProd)
+    {
+      Product product;
+        product.Id = idNum;
+        ProductList.Add(product);
+    }
 }
