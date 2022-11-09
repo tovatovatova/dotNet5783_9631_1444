aa79c92bea83dc4a4fbcaa2e4fe756d7d1c76c45
ortv <ortv617@gmail.com>
2022-11-09 22:23:41 +0200

    DalOrder and DalOrderItem

--- DalList/DalOrder.cs
+++ DalList/DalOrder.cs
@@ -1,8 +1,41 @@
 ﻿
 
+using DO;
+using static Dal.DataSource;
+
 namespace Dal;
 
 public class DalOrder
 {
+    public int Add(Order newOrder)
+    {
+        newOrder.OrderId = DataSource.Config.NextOrderNumber;
+        DataSource.OrderList.Add(newOrder);
+        return newOrder.OrderId;
+    }
+    public Order ReadById(int orderId)
+    {
+        Order? newOrder=DataSource.OrderList.Find(x => x.Value.OrderId ==orderId);    
+        if(newOrder!= null)
+        {
+            return (Order)newOrder;
 
+        }
+        throw new Exception("order not exist");
+    }
+    public IEnumerable<Order?> GetAll() => DataSource.OrderList;
+    public void Delete(int orderId)
+    {
+        Order? newOrder = DataSource.OrderList.Find(x => x.Value.OrderId == orderId);
+        if( newOrder== null)
+            throw new Exception("order not exist");
+        DataSource.OrderList.Remove(newOrder);
+    }
+    public void Update(Order newOrder)
+    {
+       int index= DataSource.OrderList.FindIndex(x => x.Value.OrderId == newOrder.OrderId);
+        if(index==-1)
+            throw new Exception("order not exist");
+        DataSource.OrderList[index] = newOrder;
+    }
 }
--- DalList/DalOrderItem.cs
+++ DalList/DalOrderItem.cs
@@ -1,8 +1,41 @@
 ﻿
 
+using DO;
+
 namespace Dal;
 
 public class DalOrderItem
 {
+    public int Add(OrderItem newOrderItem)
+    {
+        newOrderItem.OrderItemId = DataSource.Config.NextOrderItemNumber;
+        DataSource.OrderItemList.Add(newOrderItem);
+        return newOrderItem.OrderItemId;
+    }
+    public OrderItem ReadById(int orderItemId)
+    {
+        OrderItem? newOrder = DataSource.OrderItemList.Find(x => x.Value.OrderItemId == orderItemId);
+        if (newOrder != null)
+        {
+            return (OrderItem)newOrder;
 
+        }
+        throw new Exception("order item not exist");
+    }
+    public IEnumerable<OrderItem?> GetAll() => DataSource.OrderItemList;
+    public void Delete(int orderItemId)
+    {
+        OrderItem? newOrder = DataSource.OrderItemList.Find(x => x.Value.OrderId == orderItemId);
+        if (newOrder == null)
+            throw new Exception("order item not exist");
+        DataSource.OrderItemList.Remove(newOrder);
+    }
+    public void Update(OrderItem newOrder)
+    {
+        int index = DataSource.OrderItemList.FindIndex(x => x.Value.OrderItemId == newOrder.OrderItemId);
+        if (index == -1)
+            throw new Exception("order itemnot exist");
+        DataSource.OrderItemList[index] = newOrder;
+    }
 }
+
