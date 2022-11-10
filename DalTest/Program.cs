9c6bd066dc09120a55404ee32569ab1c95dd34bc
ortv <ortv617@gmail.com>
2022-11-10 20:11:00 +0200

    product options

--- DalTest/Program.cs
+++ DalTest/Program.cs
@@ -8,9 +8,9 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-      
+
         DalOrder dalOrder = new DalOrder();
-       DalOrderItem dalOrderItem = new DalOrderItem();
+        DalOrderItem dalOrderItem = new DalOrderItem();
         DalProduct dalProduct = new DalProduct();
         foreach (var item in dalOrder.GetAll())
             Console.WriteLine(item);
@@ -20,6 +20,28 @@ internal class Program
         Console.WriteLine("2: Orders");
         Console.WriteLine("3: OrderItems");
         Console.WriteLine("0: Exit");
+        int choice = System.Console.Read();
+        switch (choice)
+        {
+            case 1:
+                ProductOptions();
+                break;
+            case 2:
+                OrderOptions();
+                break;
+            case 3:
+                OrderItemOptions();
+                break;
+            default:
+                break;
+        }
+
+
+
+
+
+
+
 
         void ProductOptions()
         {
@@ -31,20 +53,84 @@ internal class Program
             Console.WriteLine("d: update product");
             Console.WriteLine("e: delete product");
             Console.WriteLine("g: change object");
-            Console.WriteLine("g: exit");
-
             ch = char.Parse(Console.ReadLine());
             while (ch != 0)
             {
+                
                 switch (ch)
                 {
-                    default:
+                    case 'a':
+
+                        string input1;
+                        int x1;
+                        double y;
+                        Product product = new Product();
+                        Console.WriteLine("enter id:");
+                        input1 = Console.ReadLine();
+                        if (int.TryParse(input1, out x1))
+                            product.Id = x1;
+                        Console.WriteLine("enter name:");
+                        product.Name = Console.ReadLine();
+                        Console.WriteLine("enter price:");
+                        input1 = Console.ReadLine();
+                        if (double.TryParse(input1, out y))
+                            product.Price = x1;
+                        Console.WriteLine("enter amount in stock:");
+                        input1 = Console.ReadLine();
+                        if (int.TryParse(input1, out x1))
+                            product.AmountInStock = x1;
+                        Console.WriteLine("enter category number:");
+                        if (int.TryParse(input1, out x1))
+                            product.ProductCategoty = (Category)x1;
+                        dalProduct.Add(product);
                         break;
-                }
-            }
+                    case 'b':
+                        string input2;
+                        int x2;
+                        Console.WriteLine("enter id:");
+                        input2 = Console.ReadLine();
+                        if (int.TryParse(input2, out x2))
+                        {
+
+                        }
+                            break;
+                            default:
+                        break;
+    }
+}
             // Console.WriteLine("");
         }
+        void OrderOptions()
+{
+    char ch;
+    Console.WriteLine("choose one of the following options:");
+    Console.WriteLine("a: Add new order");
+    Console.WriteLine("b: See order details by insert order code");
+    Console.WriteLine("c: See all orders");
+    Console.WriteLine("d: update order");
+    Console.WriteLine("e: delete order");
+    Console.WriteLine("g: change order");
 
+
+
+
+
+}
+void OrderItemOptions()
+{
+    char ch;
+    Console.WriteLine("choose one of the following options:");
+    Console.WriteLine("a: Add new order item");
+    Console.WriteLine("b: See order item details by insert order code");
+    Console.WriteLine("c: See all order items");
+    Console.WriteLine("d: update order item");
+    Console.WriteLine("e: delete order item");
+    Console.WriteLine("g: change order item");
+
+
+
+
+}
     }
 }
            
