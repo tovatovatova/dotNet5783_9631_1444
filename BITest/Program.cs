32fc21521a5387c324faa75a7bb3b23cceae02e7
טובה רוטנשטיין <trottens@g.jct.ac.il>
2022-11-23 16:34:01 +0200

    program

--- BITest/Program.cs
+++ BITest/Program.cs
@@ -4,7 +4,7 @@ namespace BITest
 {
     internal class Program
     {
-        enum Options {PRODUCT,ORDER,CART};
+        enum Options {PRODUCT=1,ORDER,CART};
         enum ProductActions {Product_List=1,ProductDetails,Add,Delete,Update,Catalog,Product_in_Catalog}
         enum CartOptions {Add=1,Update,Create }
         enum OrderOption { Get_Order=1,Order_List,Update_Ship,Update_Delivery,Order_Tracking,Update_Order}
