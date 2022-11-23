4457e30318f50b4eccf7907df4d059572da971e1
טובה רוטנשטיין <trottens@g.jct.ac.il>
2022-11-23 16:05:52 +0200

    nmain

--- BITest/Program.cs
+++ BITest/Program.cs
@@ -5,7 +5,7 @@
         enum Options {PRODUCT,ORDER,CART};
         enum ProductActions {Product_List=1,ProductDetails,Add,Delete,Update,Catalog,Product_in_Catalog}
         enum CartOptions {Add=1,Update,Create }
-        enum OrderOption { Get_Order=1,Order_List,Update_Ship,Update_Delivery}
+        enum OrderOption { Get_Order=1,Order_List,Update_Ship,Update_Delivery,Order_Tracking,Update_Order}
 
         static void Main(string[] args)
         {
