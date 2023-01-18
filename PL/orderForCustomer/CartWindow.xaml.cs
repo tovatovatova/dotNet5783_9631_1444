0b3eee377e3af9bd603d7896fc8704ab82fbe64a
ortv <ortv617@gmail.com>
2023-01-19 01:57:06 +0200

    validation

--- PL/orderForCustomer/CartWindow.xaml.cs
+++ PL/orderForCustomer/CartWindow.xaml.cs
@@ -22,10 +22,10 @@ namespace PL
     /// <summary>
     /// Interaction logic for CartWindow.xaml
     /// </summary>
-   
+
     public partial class CartWindow : Window
     {
-        BlApi.IBl bl = BlApi.Factory.Get();     
+        BlApi.IBl bl = BlApi.Factory.Get();
         public ObservableCollection<BO.OrderItem?> myOrderItems
         {
             get { return (ObservableCollection<BO.OrderItem?>)GetValue(myOrderItemsProperty); }
@@ -34,7 +34,7 @@ namespace PL
         // Using a DependencyProperty as the backing store for PlProduct.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty myOrderItemsProperty =
             DependencyProperty.Register("myOrderItems", typeof(ObservableCollection<BO.OrderItem?>), typeof(Window), new PropertyMetadata(null));
-       public BO.Cart myCart
+        public BO.Cart myCart
         {
             get { return (BO.Cart)GetValue(myCartProperty); }
             set { SetValue(myCartProperty, value); }
@@ -64,7 +64,7 @@ namespace PL
             InitializeComponent();
 
         }
-        public CartWindow( Cart cart, User user)//showing cart of submited user
+        public CartWindow(Cart cart, User user)//showing cart of submited user
         {
             myCart = cart;
             myUser = user;
@@ -74,8 +74,8 @@ namespace PL
 
         private void btnAdd_Click(object sender, RoutedEventArgs e)
         {
-            
-            e.Handled= true;
+
+            e.Handled = true;
             try
             {
                 myCart = bl.Cart.AddToCart(myCart, Convert.ToInt32((sender as Button).Tag.ToString()));
@@ -89,41 +89,41 @@ namespace PL
             {
                 (sender as Button).IsEnabled = false;
             }
-            
+
         }
 
-        
 
-       
 
-       
+
+
+
 
         private void btnLow_Click(object sender, RoutedEventArgs e)
         {
 
             BO.OrderItem item = (BO.OrderItem)(sender as Button).DataContext;
-            bl.Cart.UpdateProductInCart(myCart, item.Amount-1, item.ProductID);
+            bl.Cart.UpdateProductInCart(myCart, item.Amount - 1, item.ProductID);
             orderItemsListView.ItemsSource = myCart.Items;
             orderItemsListView.Items.Refresh();
-           txtTotalPCart.Text = "Total:"+myCart.TotalPrice.ToString()+"$";
+            txtTotalPCart.Text = "Total:" + myCart.TotalPrice.ToString() + "$";
 
 
         }
 
         private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            bl.Cart.UpdateProductInCart(myCart , 0, Convert.ToInt32((sender as TextBlock).Tag.ToString()));
+            bl.Cart.UpdateProductInCart(myCart, 0, Convert.ToInt32((sender as TextBlock).Tag.ToString()));
             orderItemsListView.ItemsSource = myCart.Items;
             orderItemsListView.Items.Refresh();
-           txtTotalPCart.Text = "Total:" + myCart.TotalPrice.ToString() + "$";
+            txtTotalPCart.Text = "Total:" + myCart.TotalPrice.ToString() + "$";
 
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            if(myCart != null&&myCart.Items.Count()!=0) { }
+            if (myCart != null && myCart.Items.Count() != 0) { }
             {
-                
+
                 grid1.Visibility = Visibility.Visible;
                 (sender as Button).IsEnabled = false;
             }
@@ -131,56 +131,44 @@ namespace PL
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            if (myCart.CustomerAddress == null || myCart.CustomerEmail == null || myCart.CustomerName == null)
+            try
             {
-                string messageBoxText = "wrong details try again";
-                string caption = "error";
-                MessageBoxImage icon = MessageBoxImage.Error;
+                (sender as Button).Visibility = Visibility.Hidden;
+                checkuot.Visibility = Visibility.Hidden;
+                int id = bl.Cart.OrderCreate(myCart);
+                string messegeBoxText = @"Your order has been successfully placed
+      Order Number: " + id;
+                string caption = " ";
+                MessageBoxImage icon = MessageBoxImage.Information;
                 MessageBoxResult result;
-                result = MessageBox.Show(messageBoxText, caption, MessageBoxButton.OK, icon, MessageBoxResult.OK);
+                result = MessageBox.Show(messegeBoxText, caption, MessageBoxButton.OK, icon, MessageBoxResult.OK);
                 if (result == MessageBoxResult.OK)
                 {
-                    return;
+                    myCart.Items = null;
+
+                    ProductCatalogWindow p = new ProductCatalogWindow(myUser, myCart);
+                    p.ShowDialog();
+                    Close();
                 }
             }
-            else
+            catch (BO.BlIdDoNotExistException)
             {
-                try
-                {
-                    (sender as Button).Visibility = Visibility.Hidden;
-                    checkuot.Visibility = Visibility.Hidden;
-                    int id = bl.Cart.OrderCreate(myCart);
-                    string messegeBoxText = @"Your order has been successfully placed
-      Order Number: " + id;
-                    string caption = " ";
-                    MessageBoxImage icon = MessageBoxImage.Information;
-                    MessageBoxResult result;
-                    result = MessageBox.Show(messegeBoxText, caption, MessageBoxButton.OK, icon, MessageBoxResult.OK);
-                    if (result == MessageBoxResult.OK)
-                    {
-                        myCart.Items = null;
-
-                        ProductCatalogWindow p = new ProductCatalogWindow(myUser, myCart);
-                        p.ShowDialog();
-                        Close();
-                    }
-                }
-                catch (BO.BlIdDoNotExistException)
-                {
-                    string messageBoxText = "something went wrong";
-                    string caption = "error";
-                    MessageBoxImage icon = MessageBoxImage.Error;
-                    MessageBoxResult result;
-                    result = MessageBox.Show(messageBoxText, caption, MessageBoxButton.OK, icon, MessageBoxResult.OK);
-                    if (result == MessageBoxResult.OK)
-                    {
-                        return;
-                    }
-                }
+                MessageBox.Show("something went wrong", "error", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK);
+                return;
             }
-
+            catch (BO.BlInvalidInputException ex)
+            {
+                MessageBox.Show("wrong details try again", "error", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK);
+            }
+            catch(BO.BlNullPropertyException ex) 
+            {
+                MessageBox.Show(ex.ToString(), "error", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK);
+                return;
+            }
         }
 
-      
     }
+
+
 }
+
